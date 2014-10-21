359c5387f64e359d6d04bfc22074460c634a927b
Steinunn <steinunn.olason@gmail.com>
2014-10-21 13:36:41 -0400

    Small refinements to make exxercise 2 & 3 neater and add comments

--- exercise2.rb
+++ exercise2.rb
@@ -1,12 +1,20 @@
+# Part 1 of exercise - calculating a tip of a 55 dollar meal
+
 meal_cost = 55
 tip = meal_cost * 0.15
 total_cost = meal_cost + tip
-puts tip # Part 1 of exercise
+puts tip 
 
-puts "You should tip #{tip}. Making the total cost #{total_cost}" # Part 2 of exercise
+# Part 2 of exercise - puts a string interpolation of the tip
 
-puts 45628*7839 # Part 3 of exercise
+puts "You should tip #{tip}. Making the total cost #{total_cost}" 
+
+# Part 3 of exercise - using Ruby as a calculator
+
+puts 45628*7839 
+
+#Part 4 of exercise - evaluating booleans
 
 puts "This value should be true."
 (true && false) || (false && true) || !(false && false)
-puts "Yes it is!" #Part 4 of exercise
+puts "Yes it is!" 
--- exercise3.rb
+++ exercise3.rb
@@ -3,5 +3,6 @@ name = gets.chomp
 puts "Hi #{name}!"
 puts "How old are you?"
 age = gets.chomp
-birth_year = 2014 - age.to_i
+birth_year = 2014 - age.to_i 
+#convert age string input to integer to calculate birth year
 puts "You were born in #{birth_year}."
